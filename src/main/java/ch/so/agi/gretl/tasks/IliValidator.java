9cad318c970a36bc1d92c4cf5bde07d655ebd2e3
edigonzales <edi.gonzales@gmail.com>
2021-07-11 20:46:09 +0200

    lazy properties

--- src/main/java/ch/so/agi/gretl/tasks/IliValidator.java
+++ src/main/java/ch/so/agi/gretl/tasks/IliValidator.java
@@ -3,6 +3,7 @@ package ch.so.agi.gretl.tasks;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.CacheableTask;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 
 import ch.so.agi.gretl.logging.GretlLogger;
@@ -14,24 +15,46 @@ public abstract class IliValidator extends AbstractValidatorTask {
     private GretlLogger log;
 
     @Input
+    @Optional
     public abstract Property<String> getDummy();
-
     
+    @Input
+    @Optional
+    public Property<String> getDisableValidation() {
+        return getProject().getObjects().property(String.class).convention("myDefault");
+    } 
+    
+//    @Input
+//    @Optional
+//    public Property<String> getDisableValidation() {
+//        return disableValidation;
+//    }
+
+//    public void setDisableValidation(Property<String> disableValidation) {
+//        this.disableValidation = disableValidation;
+//    }
+    
+    public void init() {
+    }
+
     @TaskAction
     public void validate() {
+        init();
+        
         
         //getModeldir().convention("gaga");
         
         log = LogEnvironment.getLogger(IliValidator.class);
         
-        System.out.println(getModels().get());
-        
-        System.out.println(this.greeting);
-        
-        System.out.println(getModeldir().get());
-        System.out.println(getModeldir().map(String::toUpperCase).get());
+//        System.out.println(getModels().get());
+//        
+//        System.out.println(this.greeting);
+//        
+//        System.out.println(getModeldir().get());
+//        System.out.println(getModeldir().map(String::toUpperCase).get());
         
         System.out.println(getDummy().get());
+        System.out.println(getDisableValidation().get());
 
     }
 
--- src/main/java/ch/so/agi/gretl/tasks/impl/AbstractValidatorTask.java
+++ src/main/java/ch/so/agi/gretl/tasks/impl/AbstractValidatorTask.java
@@ -4,11 +4,13 @@ import ch.ehi.basics.settings.Settings;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
 import org.interlis2.validator.Validator;
@@ -16,103 +18,121 @@ import org.interlis2.validator.Validator;
 import java.util.List;
 
 public abstract class AbstractValidatorTask extends DefaultTask {
-    //@Input
-    public String greeting = "hello from GreetingTask";
+//  @InputFiles
+//  public Object dataFiles;
     
-//    @Input
-//    public String getGreeting() {
-//        return greeting;
-//    }
-//    
-//    public void setGreeting(String greeting) {
-//        this.greeting = greeting;
-//    }
-
-
     @Input
     @Optional
     public abstract Property<String> getModels();
     
     @Input
+    @Optional
     public abstract Property<String> getModeldir();
     
+    @InputFile
+    @Optional
+    public abstract RegularFileProperty getConfigFile();
     
-//    @InputFiles
-//    public Object dataFiles;
-//    @Input
-//    @Optional
-//    public String models = null;
-//    @Input
-//    @Optional
-//    public String modeldir = null;
-//    @InputFile
-//    @Optional
-//    public Object configFile = null;
-//    @Input
-//    @Optional
-//    public boolean forceTypeValidation = false;
-//    @Input
-//    @Optional
-//    public boolean disableAreaValidation = false;
-//    @Input
-//    @Optional
-//    public boolean multiplicityOff = false;
-//    @Input
-//    @Optional
-//    public boolean allObjectsAccessible = false;
-//    @Input
-//    @Optional
-//    public boolean skipPolygonBuilding = false;
-//    @OutputFile
-//    @Optional
-//    public Object logFile = null;
-//    @OutputFile
-//    @Optional
-//    public Object xtflogFile = null;
+    public Property<Boolean> forceTypeValidation = getProject().getObjects().property(Boolean.class).convention(false); 
+    
+    @Input
+    @Optional
+    public Property<Boolean> getForceTypeValidation() {
+        return forceTypeValidation;
+    }
+
+    public Property<Boolean> disableAreaValidation = getProject().getObjects().property(Boolean.class).convention(false); 
+
+    @Input
+    @Optional
+    public Property<Boolean> getDisableAreaValidation() {
+        return disableAreaValidation;
+    }
+
+    public Property<Boolean> multiplicityOff = getProject().getObjects().property(Boolean.class).convention(false); 
+
+    @Input
+    @Optional
+    public Property<Boolean> getMultiplicityOff() {
+        return multiplicityOff;
+    }
+    
+    public Property<Boolean> allObjectsAccessible = getProject().getObjects().property(Boolean.class).convention(false); 
+
+    @Input
+    @Optional
+    public Property<Boolean> getAllObjectsAccessible() {
+        return allObjectsAccessible;
+    }
+    
+    public Property<Boolean> skipPolygonBuilding = getProject().getObjects().property(Boolean.class).convention(false); 
+
+    @Input
+    @Optional
+    public Property<Boolean> getSkipPolygonBuilding() {
+        return skipPolygonBuilding;
+    }
+
+    @OutputFile
+    @Optional
+    public abstract RegularFileProperty getLogFile();
+    
+    @OutputFile
+    @Optional
+    public abstract RegularFileProperty getXtfLogFile();
+
 //    @InputDirectory
 //    @Optional
 //    public Object pluginFolder = null;
-//    @Input
-//    @Optional
-//    public String proxy = null;
-//    @Input
-//    @Optional
-//    public Integer proxyPort = null;
-//    @Input
-//    @Optional
-//    public boolean failOnError = true;
-//    public boolean validationOk = true;
+    
+    @Input
+    @Optional
+    public abstract Property<String> getProxy();
+
+    @Input
+    @Optional
+    public abstract Property<Integer> getProxyPort();
+    
+    public Property<Boolean> failOnError = getProject().getObjects().property(Boolean.class).convention(true); 
+
+    @Input
+    @Optional
+    public Property<Boolean> getFailOnError() {
+        return failOnError;
+    }
+
+    private boolean validationOk = true;
 
     protected void initSettings(Settings settings) {
         
         
         settings.setValue(Validator.SETTING_DISABLE_STD_LOGGER, Validator.TRUE);
-//        if (models != null) {
-//            settings.setValue(Validator.SETTING_MODELNAMES, models);
-//        }
-//        if (modeldir != null) {
-//            settings.setValue(Validator.SETTING_ILIDIRS, modeldir);
-//        }
-//        if (configFile != null) {
-//            settings.setValue(Validator.SETTING_CONFIGFILE, this.getProject().file(configFile).getPath());
-//        }
-//        if (forceTypeValidation) {
-//            settings.setValue(Validator.SETTING_FORCE_TYPE_VALIDATION, Validator.TRUE);
-//        }
-//        if (disableAreaValidation) {
-//            settings.setValue(Validator.SETTING_DISABLE_AREA_VALIDATION, Validator.TRUE);
-//        }
-//        if (multiplicityOff) {
-//            settings.setValue(Validator.SETTING_MULTIPLICITY_VALIDATION,
-//                    ch.interlis.iox_j.validator.ValidationConfig.OFF);
-//        }
-//        if (allObjectsAccessible) {
-//            settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
-//        }
-//        if (skipPolygonBuilding) {
-//            settings.setValue(ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_LINETABLES,
-//                    ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_LINETABLES_DO);
-//        }
+        if (getModels().get() != null) {
+            settings.setValue(Validator.SETTING_MODELNAMES, getModels().get());
+        }
+        if (getModeldir().get() != null) {
+            settings.setValue(Validator.SETTING_ILIDIRS, getModeldir().get());
+        }
+        if (getConfigFile().get() != null) {
+            settings.setValue(Validator.SETTING_CONFIGFILE, getConfigFile().get().getAsFile().getPath());
+        }
+        if (forceTypeValidation.get()) {
+            settings.setValue(Validator.SETTING_FORCE_TYPE_VALIDATION, Validator.TRUE);
+        }
+        if (disableAreaValidation.get()) {
+            settings.setValue(Validator.SETTING_DISABLE_AREA_VALIDATION, Validator.TRUE);
+        }
+        if (multiplicityOff.get()) {
+            settings.setValue(Validator.SETTING_MULTIPLICITY_VALIDATION,
+                    ch.interlis.iox_j.validator.ValidationConfig.OFF);
+        }
+        if (allObjectsAccessible.get()) {
+            settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        }
+        if (skipPolygonBuilding.get()) {
+            settings.setValue(ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_LINETABLES,
+                    ch.interlis.iox_j.validator.Validator.CONFIG_DO_ITF_LINETABLES_DO);
+        }
 //        if (logFile != null) {
 //            settings.setValue(Validator.SETTING_LOGFILE, this.getProject().file(logFile).getPath());
 //        }
